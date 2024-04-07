4900bfd451d814690fff804f1302a2a6beb669b4
Prince Kumar <58019458+kprince99@users.noreply.github.com>
2024-04-07 15:48:48 +0530

    Update server.js

--- api/server.js
+++ api/server.js
@@ -1,1 +1,12 @@
-hii
+const express = require('express');
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+                                            
+// middlewares
+app.use(express.json({ extended: false }));
+
+app.use('/payment', require('./routes/payment'));
+
+app.listen(port, () => console.log(`server started on port ${port}`));
